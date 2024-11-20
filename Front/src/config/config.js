8add79fdeb7c4038cbbc14159f0f5c500c99c6f0
Nains <jason20226@gmail.com>
2024-11-20 10:43:13 +0800

    config 修正

--- Front/src/config/config.js
+++ Front/src/config/config.js
@@ -2,13 +2,13 @@ function env() {
     const _DEFAULT = {
         webAPI: "AKfycbwgVIScdFRncfGNACfBixqwveHchctpxTK0j-63Ci1BfMWV31uCuN64pgQ1GSQtuC3i"
     };
+    const Location = "mhrbot.zeabur";
+    try {
+      _DEFAULT.url = Location ? "https://script.google.com/" : "";
+    } catch (e) {
+      console.log(`Config url Error: ${e}`);
+    }
     return _DEFAULT;
-  }
-  const Location = "mhrbot.zeabur";
-  try {
-    _DEFAULT.url = Location ? "https://script.google.com/" : "";
-  } catch (e) {
-    console.log(`Config url Error: ${e}`);
   }
   window["env"] = env();
   
