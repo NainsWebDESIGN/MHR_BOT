dc0b8e61316cabdd5497cd47ce36e7d99b564cd0
Nains <jason20226@gmail.com>
2024-11-26 14:31:17 +0800

    2024-11-26 14:31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,21 +12,6 @@ export class AppComponent implements OnInit {
   }
   ngOnInit() {
     // this.api.getData().subscribe(res => this.title = (typeof res == "string") ? this.changeData(res) : res);
-    // this.api.getGAS("post", GateWay.LOGIN, "login", { username: "Nains", password: "1234" })
-    //   .subscribe(res => console.log(res));
-    // this.api.getGAS("get", GateWay.GET)
-    //   .subscribe(res => {
-    //     this.title = res.map(item => {
-    //       return {
-    //         skill1: item[0],
-    //         skill2: item[2],
-    //         point1: item[1],
-    //         point2: item[3],
-    //         hole: item[4]
-    //       };
-    //     })
-    //     console.log(this.title);
-    //   });
   }
   changeData(res) {
     const box = res.split("_").map(item => {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,10 +7,24 @@ import { GateWay } from '@ts/enum';
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
+  title;
   username: string = "";
   password: string = "";
   constructor(private api: ApiService) { }
   ngOnInit() {
+    // this.api.getGAS("get", GateWay.GET)
+    //   .subscribe(res => {
+    //     this.title = res.map(item => {
+    //       return {
+    //         skill1: item[0],
+    //         skill2: item[2],
+    //         point1: item[1],
+    //         point2: item[3],
+    //         hole: item[4]
+    //       };
+    //     })
+    //     console.log(this.title);
+    //   });
   }
   Login() {
     this.api.getGAS("post", GateWay.LOGIN, "login", { username: this.username, password: this.password })
