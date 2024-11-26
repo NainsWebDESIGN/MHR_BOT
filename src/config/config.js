d92489ed5e94b72133b15eedfb8ca8a9d31ee251
Nains <jason20226@gmail.com>
2024-11-26 14:42:43 +0800

    update config webAPI

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 function env() {
     const _DEFAULT = {
         url: "",
-        webAPI: "AKfycbxKbS3wYTy-JjRUqrfs8lcgFYJNYfM6D02hE8iI3GtUSQG-9D7z5dyZ3mss06N5Irpx",
+        webAPI: "AKfycbzUl4MvUtfKg6BUUsGcjN3kELeZkpC-EBL1Iof_XN957vwsd-333uwMfvCIv5cz-Dm9",
         loadStyle: ['https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'],
         loadScript: []
     };
